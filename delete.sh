9f42f83c675bd758869434efe0ebb6dd7b3a74fc
jzamora87 <116679971+jzamora87@users.noreply.github.com>
2025-01-28 11:28:23 -0600

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -5,6 +5,6 @@ rm -vfr /Descargas/* >/dev/null 2>&1 && rm -vfr /home/agente_r3/Descargas/* >/de
 rm -vfr /Escritorio/* >/dev/null 2>&1 && rm -vfr /home/agente_r3/Escritorio/* >/dev/null 2>&1
 #PARA USER STANDAR
 rm -vfr /tmp/* >/dev/null 2>&1 && rm -vfr /var/tmp/* >/dev/null 2>&1
-rm -vfr /Descargas/* >/dev/null 2>&1 && rm -vfr /home/user_r3/Descargas/* >/dev/null 2>&1
+rm -vfr /Descargas/* >/dev/null 2>&1 && rm -vfr /home/user_r33/Descargas/* >/dev/null 2>&1
 rm -vfr /Escritorio/* >/dev/null 2>&1 && rm -vfr /home/user_r3/Escritorio/* >/dev/null 2>&1
 
